36d72fa3b379ad881739fd1c7f26855fa28396d0
ค૪૯Ն <axelmarcial151003@gmail.com>
2023-11-28 22:45:01 +0100

    ajout app.js

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,9 +6,9 @@
   <header>
     <h1 class="nav-logo"><router-link class="not-text-link" :to="{ name: 'Home' }"><span>Axel</span><span>Marcial</span></router-link></h1>
     <nav>
-      <router-link class="not-text-link" :to="{ name: 'About' }">About</router-link>
-      <router-link class="not-text-link" :to="{ name: 'Portfolio' }">Portfolio</router-link>
-      <router-link class="not-text-link" :to="{ name: 'Contact' }">Contact</router-link>
+      <router-link class="not-text-link link-hover" :to="{ name: 'About' }">About</router-link>
+      <router-link class="not-text-link link-hover" :to="{ name: 'Portfolio' }">Portfolio</router-link>
+      <router-link class="not-text-link link-hover" :to="{ name: 'Contact' }">Contact</router-link>
     </nav>
     <div class="switch-container"></div>
   </header>
@@ -35,9 +35,15 @@
     position: relative;
     left: 10px;
   }
+  nav{
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+  }
   nav a{
     font-size: var(--fs-nav-link);
     font-weight: var(--fw-medium);
     text-transform: uppercase;
   }
+
 </style>
