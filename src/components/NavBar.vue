ca11ba1e57f7085a28231285a232e558b6816077
ค૪૯Ն <axelmarcial151003@gmail.com>
2023-11-28 22:28:09 +0100

    mise en place du router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1 class="nav-logo"><router-link class="not-text-link" :to="{ name: 'Home' }"><span>Axel</span><span>Marcial</span></router-link></h1>
+    <nav>
+      <router-link class="not-text-link" :to="{ name: 'About' }">About</router-link>
+      <router-link class="not-text-link" :to="{ name: 'Portfolio' }">Portfolio</router-link>
+      <router-link class="not-text-link" :to="{ name: 'Contact' }">Contact</router-link>
+    </nav>
+    <div class="switch-container"></div>
+  </header>
+</template>
+
+<style scoped>
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: var(--pad-nav-top) var(--pad-nav-x);
+  }
+  h1 a{
+    display: flex;
+    gap: 1rem;
+    font-size: var(--fs-nav-logo);
+    text-transform: uppercase;
+    font-weight: var(--fw-extrabold);
+  }
+  h1 span{
+    line-height: 77%;
+  }
+  h1 span:first-child{
+    position: relative;
+    left: 10px;
+  }
+  nav a{
+    font-size: var(--fs-nav-link);
+    font-weight: var(--fw-medium);
+    text-transform: uppercase;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import NavBar from '../components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <h1>Home</h1>
+</template>
+
+<style scoped>
+
+</style>
